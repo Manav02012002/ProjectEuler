2e2d4faf90b664fe4447ba6ba22ef45b33caa0d4
Manav Madan Rawal <80583498+Manav02012002@users.noreply.github.com>
2022-04-27 08:05:02 +0530

    Add files via upload

---
+++ Euler14.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Euler14 {
+
+	public static void main(String[] args) {
+
+		int limit = 1000000; 
+		int size = 0, max = 1, j = 0;
+		for (int i = 2; i < limit; i++) {
+			size = getChainSize(i);
+			if (size > max) {
+				max = size;
+				j = i;
+			}
+		}
+		System.out.println( j);
+	}
+
+	private static int getChainSize(int n) {
+
+		long num = n;
+		List<Long> list = new ArrayList<>();
+		int size = 0;
+		while (num != 1) {
+			list.add(num);
+
+			if (num % 2 == 0) {
+				num = num / 2;
+			} else {
+				num = 3 * num + 1;
+			}
+		}
+		size = list.size();
+		return size;
+	}
+
+}
---
+++ Euler15.java
@@ -0,0 +1,36 @@
+import java.io.*;
+class Euler15 {
+
+  
+  public static void main (String[] args) throws Exception {
+
+    int size = 21;
+
+    long[][] pts=new long[size][size];
+
+    pts[0][0] = 1;
+    for(int i = 1; i < size; i++) {
+      pts[i][0] = 1;
+      pts[0][i] = 1;
+    }
+
+    for(int r = 1; r < size; r++) {
+      pts[r][r] = pts[r-1][r] + pts[r][r-1];
+      
+      for(int c = r + 1; c < size; c++) {
+        pts[r][c] = pts[r-1][c] + pts[r][c-1];
+        pts[c][r] = pts[c-1][r] + pts[c][r-1];
+      }
+    }
+    
+    long paths = pts[size-1][size-1];
+    
+    System.out.println(paths);
+    for(int i = 0; i < size; i++) {
+      for(int j = 0; j < size; j++) {
+        System.out.println(pts[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
